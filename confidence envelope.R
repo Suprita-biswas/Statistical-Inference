1807d5cf19b9238b4307c24ccb31d7163f667c4f
Suprita Biswas <53855514+Suprita-biswas@users.noreply.github.com>
2020-03-03 22:10:36 +0530

    Add files via upload
    Data on the magnitudes of earthquakes near Fiji are available at http://www.stat.cmu.edu/~larry/
    all-of-statistics/=data/fijiquakes.dat. Estimate the cdf F for earthquake magnitudes. Com-
    pute and plot a 95% condence envelope for F. Find an approximate 95% condence interval for
     = F(4:9) 􀀀 F(4:3).

---
+++ confidence envelope.R
@@ -0,0 +1,22 @@
+
+x<-read.table("earthquake.txt",header=FALSE)
+
+mag<-x[[5]]
+mag<-sort(mag)
+
+n<-length(mag)
+a<-0.05
+ep<-sqrt((log(2/a))/(2*n))
+
+
+plot(ecdf(mag),xlab="Earthquake magnitude",ylab="Fn(mag)")
+U<-NULL
+L<-NULL
+for(i in 1:length(mag)){
+
+U[i]<-min(ecdf(mag)(mag[i])+ep,1)
+L[i]<-max(ecdf(mag)(mag[i])-ep,0)}
+
+lines(mag, U, col="blue")
+
+lines(mag, L, col="blue")
